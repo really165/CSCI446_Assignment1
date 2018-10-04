021b51cd98cecf64862d2417dc8b192b0c1125a0
really165 <31826004+really165@users.noreply.github.com>
2018-10-04 16:57:53 -0600

    Added node class to make it easier to push and pop stuff off of a stack
    I'll really only be using it to store the points for the DFS search but it might be useful in the other search classes if modified.

---
+++ Node.java
@@ -0,0 +1,19 @@
+package assignment1;
+
+public class Node {
+    int x;
+    int y;
+    
+    public Node(int xPos, int yPos){
+        x = xPos;
+        y = yPos;
+    }
+    
+    public int getX(){
+        return x;
+    }
+    
+    public int getY(){
+        return y;
+    }
+}
