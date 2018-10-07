5cd1d1840e57ca9be0ca2e8dc6bf528fb718da96
really165 <31826004+really165@users.noreply.github.com>
2018-10-07 00:53:12 -0600

    Is now able to solve all mazes
    Making a Node class was a fucking mistake and I'm sorry.

--- DFS.java
+++ DFS.java
@@ -12,8 +12,13 @@ public class DFS {
     int cost = 0;
     int nodes = 0;
     char dash = "-".charAt(0);
-    Stack<Node> stack = new Stack<>();
-    Stack<Node> output = new Stack<>();
+    char period = ".".charAt(0);
+    char finish = "*".charAt(0);
+    Node currentPosition;
+    Stack<Integer> stackX = new Stack<>();
+    Stack<Integer> stackY = new Stack<>();
+    Stack<Integer> outputX = new Stack<>();
+    Stack<Integer> outputY = new Stack<>();
     
     public DFS(char[][] input){
         maze = input;
@@ -30,29 +35,149 @@ public class DFS {
                 }
             }
         }
+        //make the current position into a node
+        currentPosition = new Node(posX, posY);
+        //put starting point on the stack
+        stackX.push(currentPosition.x);
+        stackY.push(currentPosition.y);
+        //visit starting position
+        visit(currentPosition);
+        printMaze(maze);
     }
     
     public void traverseMaze(){
-        //put starting point on the stack
-        Node startingPosition = new Node(posX, posY);
-        stack.push(startingPosition);
-        System.out.println("Next we will move " + nextMove());
+        while(!solved()){
+            move();
+        }
+    }
+    
+    public void move(){
+        //determine what to do next
+        //if position above is available
+        if(checkUp()){
+            moveUp();
+        }
+        //if position below is available
+        else if(checkDown()){
+            moveDown();
+        }
+        //if position left is available
+        else if(checkLeft()){
+            moveLeft();
+        }
+        //if position right is available
+        else if(checkRight()){
+            moveRight();
+        }
+        //else pop current position off stack
+        else{
+            popFromStack();
+            if(moveIsAvailable()){
+                move();
+            }
+        }
+        printMaze(maze);
+    }
+    
+    public void backtrack(){
+        if(maze[posX-1][posY]==period){
+            
+        }
+    }
+    
+    public boolean moveIsAvailable(){
+        if(checkUp()){
+            return true;
+        }
+        else if(checkDown()){
+            return true;
+        }
+        else if(checkLeft()){
+            return true;
+        }
+        else if(checkRight()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    public boolean solved(){
+        //check up
+        if(maze[posX-1][posY]==finish){
+            System.out.println("solved");
+            return true;
+        }
+        //check down
+        else if(maze[posX+1][posY]==finish){
+            System.out.println("solved");
+            return true;
+        }
+        //check left
+        else if(maze[posX][posY-1]==finish){
+            System.out.println("solved");
+            return true;
+        }
+        //check right
+        else if(maze[posX][posY+1]==finish){
+            System.out.println("solved");
+            return true;
+        }
+        else{
+            return false;
+        }
     }
     
     public void moveUp(){
+        //change position
         posX--;
+        //update the current position
+        update(currentPosition);
+        //push position onto stack
+        addToStack();
+        //add to output sequence
+        addToOutput();
+        //mark as visited
+        visit(currentPosition);
     }
     
     public void moveDown(){
+        //change position
         posX++;
+        //update the current position
+        update(currentPosition);
+        //push position onto stack
+        addToStack();
+        //add to output sequence
+        addToOutput();
+        //mark as visited
+        visit(currentPosition);
     }
     
     public void moveLeft(){
         posY--;
+        //update the current position
+        update(currentPosition);
+        //push position onto stack
+        addToStack();
+        //add to output sequence
+        addToOutput();
+        //mark as visited
+        visit(currentPosition);
     }
     
     public void moveRight(){
+        //change position
         posY++;
+        //update the current position
+        update(currentPosition);
+        //push position onto stack
+        addToStack();
+        //add to output sequence
+        addToOutput();
+        //mark as visited
+        visit(currentPosition);
     }
     
     public boolean checkUp(){
@@ -71,25 +196,49 @@ public class DFS {
         return (!visited[posX][posY+1])&&(maze[posX][posY+1]==dash);
     }
     
-    public String nextMove(){
-        String next;
-        if(checkUp()){
-            next = "up";
+    public void visit(Node currentPosition){
+        visited[currentPosition.x][currentPosition.y] = true;
+        maze[currentPosition.x][currentPosition.y] = period;
+    }
+    
+    public void update(Node position){
+        position.x = posX;
+        position.y = posY;
+    }
+    
+    public void popFromStack(){
+        checkPosition();
+        stackX.pop();
+        stackY.pop();
+        //change to that position
+        posX = stackX.peek();
+        posY = stackY.peek();
+        //change the current node
+        update(currentPosition);
+        checkPosition();
+    }
+    
+    public void addToStack(){
+        stackX.push(currentPosition.x);
+        stackY.push(currentPosition.y);
+    }
+    
+    public void addToOutput(){
+        outputX.push(currentPosition.x);
+        outputY.push(currentPosition.y);
+    }
+    
+    public void printMaze(char[][] maze){
+        for(int i = 0; i < maze.length; i++){
+            //make a string for the row
+            String row = "";
+            for(int j = 0; j < maze[0].length; j++){
+                char c=maze[i][j];    
+                row+=Character.toString(c);  
+            }
+            //print out row
+            System.out.println(row);
         }
-        else if(checkDown()){
-            next = "down";
-        }
-        else if(checkLeft()){
-            next = "left";
-        }
-        else if(checkRight()){
-            next = "right";
-        }
-        else{
-            //no moves; must pop from the stack
-            next = "pop";
-        }
-        return next;
     }
     
     //for debugging
